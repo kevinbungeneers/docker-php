9ddb8cd1d32ce1ad2320ecf6311aa7373d2bfbd5
Kevin Bungeneers <kevin.bungeneers@gmail.com>
2016-02-17 22:14:28 +0100

    Finetuning

--- 7.0/fpm/docker-entrypoint.sh
+++ 7.0/fpm/docker-entrypoint.sh
@@ -5,7 +5,7 @@ cp /config/php.ini-$ENVIRONMENT /usr/local/etc/php/php.ini
 if [[ "$ENVIRONMENT" == 'development' ]]; then
     docker-php-ext-enable xdebug
     sed -i "s|^xdebug.idekey=\"docker-php:7.0-fpm\"$|xdebug.idekey=\"$XDEBUG_IDEKEY\"|g" /usr/local/etc/php/php.ini
-    sed -i "s|^xdebug.remote_port=9009$|xdebug.remote_port=$XDEBUG_REMOTE_PORT|g" /usr/local/etc/php/php.ini
+    sed -i "s|^xdebug.remote_port=9001$|xdebug.remote_port=$XDEBUG_REMOTE_PORT|g" /usr/local/etc/php/php.ini
 fi
 
 sed -i "s|^;date.timezone =$|date.timezone = \"$TIMEZONE\"|g" /usr/local/etc/php/php.ini
