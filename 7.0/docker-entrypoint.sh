1d50fef4d82949d8f52e8418ec947e4f551fca43
Kevin Bungeneers <kevin.bungeneers@gmail.com>
2016-02-18 22:25:19 +0100

    WIP: enable xdebug for PHP 7.0 CLI image

--- 7.0/docker-entrypoint.sh
+++ 7.0/docker-entrypoint.sh
@@ -2,6 +2,16 @@
 
 cp /config/php.ini-$ENVIRONMENT /usr/local/etc/php/php.ini
 
+if [[ "$ENVIRONMENT" == 'development' ]]; then
+    docker-php-ext-enable xdebug
+    export PHP_IDE_CONFIG="serverName=$XDEBUG_SERVER_NAME"
+    #export XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_port=9001 remote_host=172.17.42.1 remote_connect_back=0"
+
+    sed -i "s|^xdebug.idekey=\"docker-php:7.0-fpm\"$|xdebug.idekey=\"$XDEBUG_IDEKEY\"|g" /usr/local/etc/php/php.ini
+    sed -i "s|^xdebug.remote_port=9001$|xdebug.remote_port=$XDEBUG_REMOTE_PORT|g" /usr/local/etc/php/php.ini
+fi
+
+
 sed -i "s|^;date.timezone =$|date.timezone = \"$TIMEZONE\"|g" /usr/local/etc/php/php.ini
 sed -i "s|^error_reporting = E_ALL$|error_reporting = $ERROR_REPORTING|g" /usr/local/etc/php/php.ini
 sed -i "s|^memory_limit = 128M$|memory_limit = $MEMORY_LIMIT|g" /usr/local/etc/php/php.ini
